3153039ccfd1cbfd5d1051a73e8bdba7f2e2a3c6
MFejeni <mfejeni@gmail.com>
2023-03-16 02:39:15 -0700

    size is not grandeur

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+/* Main, returns 0*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,")
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* Main block, returns 0*/
+int main(void)
+{
+	char c;
+	int i;
+	long int I;
+	long long int Ii;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unassigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unassigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unassigned long)sizeof(I));
+	printf("Size of a long long int: %lu byte(s)\n", (unassigned long)sizeof(Ii));
+	printf("Size of a float: %lu byte(s)\n", (unassigned long)sizeof(f));
+	return 0
+}
