fc81244a6c9e24dbe6ade84851668a07501251c9
MFejeni <mfejeni@gmail.com>
2023-03-19 04:21:34 -0700

    Inventing is a combination of brains and materials

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints combination of 2 digits
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int tenth = 48;
+	int units = 49;
+
+	while (tenth <= 56)
+	{
+		while (units <= 57)
+		{
+			if (units > tenth)
+			{
+				putchar(tenth);
+				putchar(units);
+
+				if (tenth != 56 && units != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
