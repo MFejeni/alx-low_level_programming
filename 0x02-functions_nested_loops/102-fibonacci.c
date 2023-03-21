54886096b7b7ee1528cc0607a87b008280a4dee0
MFejeni <mfejeni@gmail.com>
2023-03-21 11:52:05 -0700

    Fibo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,15 +7,18 @@
 
 int main(void)
 {
-	int fib1, fib2, i;
+	int fib0, fib1, fib2, i;
 
-	fib1 = 0;
-	fib2 = 1;
-	for (i = 0; i <= 50; i++)
+	fib1 = 1;
+	fib2 = 2;
+	fib0 = 0;
+	printf("%d", fib1);
+	for (i = 1; i <= 50; i++)
 	{
-		fib2 = fib2 + fib1;
 		printf("%d, ", fib2);
-		fib1 = fib2;
+		fib0 = fib2;
+		fib2 = fib1 + fib2;
+		fib1 = fib0;
 	}
 	putchar('\n');
 	return (0);
