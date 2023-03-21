4bcaf657372db62b2b32c08a5f0ca2898d8695b8
MFejeni <mfejeni@gmail.com>
2023-03-21 11:57:34 -0700

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,10 +14,10 @@ int main(void)
 	fib1 = 1;
 	fib2 = 2;
 	fib0 = 0;
-	printf("%d, ", fib1);
+	printf("%lu, ", fib1);
 	for (i = 1; i <= 50; i++)
 	{
-		printf("%d, ", fib2);
+		printf("%lu, ", fib2);
 		fib0 = fib2;
 		fib2 = fib1 + fib2;
 		fib1 = fib0;
