54177f3f58a6050b8537d498a479de0c1735b975
MFejeni <mfejeni@gmail.com>
2023-03-21 11:08:10 -0700

    Natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of multiples
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int sum, i;
+
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if(i % 3 == 0 && (i % 5 != 0))
+			sum = sum + i;
+		else if (i % 5 == 0 || i % 3 == 0)
+			sum = sum + i;
+	}
+	printf("%d\n", sum);
+}
