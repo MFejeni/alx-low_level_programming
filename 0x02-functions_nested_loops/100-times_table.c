dc0eeecbe3f47f21c23cabc0edec4aa36a9e173a
MFejeni <mfejeni@gmail.com>
2023-03-21 06:10:20 -0700

    n-by timetable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,7 +18,10 @@ void print_times_table(int n)
 			{
 				if (col != n)
 				{
-					printf("%d,\t", row * col);
+					printf("%d,", row * col);
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
 				}
 				else
 				{
