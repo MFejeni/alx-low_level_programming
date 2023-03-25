93ec89e61568a870a6206ee79d90d41542d3b5f9
MFejeni <mfejeni@gmail.com>
2023-03-25 04:04:29 -0700

    isdigit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,6 +3,7 @@
 /**
  * _isupper - returns 1 if a letter is upper case
  * @c: letter to check
+ * Return: 1 if uppercase 0 otherwise
  */
 
 int _isupper(int c)
--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,11 +3,12 @@
 /**
  * _isdigit - returns 1 if it is a digit
  * @c: digit to check
+ * Return: 1 if c is digit 0 otherwise
  */
 
 int _isdigit(int c)
 {
-	if (c >=48 and c <= 57)
+	if (c >= 48 and c <= 57)
 		return (1);
 	else
 		return (0);
