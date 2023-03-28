e5142fa3dcb7938a0f63f0bfa315d25c82bc7577
MFejeni <mfejeni@gmail.com>
2023-03-28 11:01:27 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies src string to dest
+ * @dest: to return after copied
+ * @src: to copy from
+ * Return: char from dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = &src;
+	return (dest);
+}
