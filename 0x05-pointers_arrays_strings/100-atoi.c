18043649d432bf78d5dd7b60646b3df7d3cc1c09
MFejeni <mfejeni@gmail.com>
2023-03-28 12:01:15 -0700

    string to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -21,7 +21,7 @@ int _atoi(char *s)
 		       result = -1 * result * 10 - (s[i] - '0');
 		else if (s[i] == '+' && (s[i] > '0' && s[i] <= '9'))
 			result = result * 10 - (s[i] - '0');
-		else if (s[i] >= '0' && s[i] <= '9')
+		else if (s[i] >= '0' && s[i] <= '9' && s[i] != '\0')
 			result = result * 10 - (s[i] - '0');
 	}
 	return (result);
