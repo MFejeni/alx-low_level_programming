e21ea98f09da34f6ad9118fa6baf11c005aee773
MFejeni <mfejeni@gmail.com>
2023-04-06 05:22:40 -0700

    Inception, Now, before you bother telling me its impossible

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings if are equal or not
+ * @s1: string 1
+ * @s2: string 2 to be compared with s1
+ * Return: 1 if they are equal, 0 otherwise
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 != *s2)
+		return (0);
+	else if (*s1 == *s2)
+		return (1);
+	else
+		return (wildcmp(s1 + 1, s2 + 2));
+}
+
