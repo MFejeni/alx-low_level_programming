33a4874014737309cc02f905a9fb1eb80d4fff52
MFejeni <mfejeni@gmail.com>
2023-04-06 06:26:57 -0700

    wild cat

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,7 +11,7 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 != *s2)
 		return (0);
-	else if (*s1 == *s2)
+	else if (*s1 == *s2 || *s1 == '*' || *s2 == '*')
 		return (1);
 	else
 		return (wildcmp(s1 + 1, s2 + 1));
