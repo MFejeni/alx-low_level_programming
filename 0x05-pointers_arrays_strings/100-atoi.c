5b48f79dc5f8eea976a52d6f2e740fb88bbcabf4
MFejeni <mfejeni@gmail.com>
2023-04-10 05:05:00 -0700

    Explanation

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,8 +17,10 @@ int _atoi(char *s)
 
 	for (i = 0; i < strlen; i++)
 	{
-		if (s[i] == '-' && (s[i] > '0' && s[i] <= '9'))
+		if((s[i] != '\0' && s[i] == '-') && (s[i] > '0' && s[i] <= '9'))
+		{
 		       result = -1 * result * 10 - (s[i] - '0');
+		}
 		else if (s[i] == '+' && (s[i] > '0' && s[i] <= '9'))
 			result = result * 10 - (s[i] - '0');
 		else if (s[i] >= '0' && s[i] <= '9' && s[i] != '\0')
