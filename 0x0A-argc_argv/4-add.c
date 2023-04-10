6ddba4c3cd732fb9ed7a0efac316f274f33523a0
MFejeni <mfejeni@gmail.com>
2023-04-10 12:36:51 -0700

    Infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -34,17 +34,3 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
-
-/**
- * _isdigit - checkes if a char is a digit
- * @c: char to check
- * Return: 1 if true, 0 otherwise
- */
-
-int _isdigit(char c)
-{
-	if (c >= 48 && c<=57)
-		return (1);
-	else
-		return (0);
-}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,7 +3,5 @@
 
 int _putchar(char c);
 int main(int argc, char *argv[]);
-int _isdigit(char c);
-
 
 #endif
