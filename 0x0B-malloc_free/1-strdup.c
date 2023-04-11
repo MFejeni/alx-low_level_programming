2d821610451d94298c5240e6cf2c4c0b0b1852f8
MFejeni <mfejeni@gmail.com>
2023-04-11 13:02:25 -0700

    If dreamt of beating me better wake up and apologize

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,7 +11,7 @@ char *_strdup(char *str)
 {
 	char *dupstr;
 	int i, strlen;
-	
+
 	if (str == NULL)
 		return (NULL);
 
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,7 +12,7 @@ int **alloc_grid(int width, int height)
 {
 	int i, j, **res;
 
-	if (width <= 0 || height <=0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	res = malloc(sizeof(int *) * height);
