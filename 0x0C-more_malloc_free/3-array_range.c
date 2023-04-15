0db20d48a53f58a2466e58d8759fecca862aa651
MFejeni <mfejeni@gmail.com>
2023-04-15 06:33:54 -0700

    array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,11 +16,11 @@ int *array_range(int min, int max)
 	arrlen = max - min + 1;
 	if (min > max)
 		return (NULL);
-	array_ptr = malloc(sizeof(int *) * (arrlen));
+	array_ptr = malloc(sizeof(int) * (arrlen));
 	if (array_ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i <= arrlen; i++)
+	for (i = 0; i < arrlen; i++)
 		array_ptr[i] = min++;
 	return (array_ptr);
 }
