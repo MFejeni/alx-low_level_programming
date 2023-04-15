efd0bb43e1410a6a94190ce3c1c3fefdfccedaf7
MFejeni <mfejeni@gmail.com>
2023-04-15 07:03:56 -0700

    _realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocates memory using malloc and free
+ * @ptr: pointer previously allocated
+ * @old_size: old size of a memory
+ * @new_size: new size of a memory
+ * Retur: NULL if function fails, otherwise ptr
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *locptr;
+	unsigned int i, j;
+
+	j = new_size;
+	if (old_size == new_size)
+		return (ptr);
+	else if (ptr == NULL)
+	{
+		locptr = malloc(new_size);
+		return (locptr);
+	}
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	locptr = malloc(new_size);
+	if (locptr == NULL)
+		return (NULL);
+	if (new_size > old_size)
+		j = old_size;
+	for (i = 0; i < j; i++)
+		locptr[i] = ptr[i];
+	free(ptr);
+	return (locptr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,6 +6,7 @@ char *string_nconcat(char *s1, char *c2, unsigned int n);
 int main(void);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 
 
