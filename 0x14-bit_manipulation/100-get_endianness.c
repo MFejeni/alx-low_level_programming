b8436d40e14eadbc6075e766045d09ab67f511f3
MFejeni <mfejeni@gmail.com>
2023-05-06 08:26:10 -0700

    endian

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * get_endianness - determines if endianness is litte or big
+ * Return: 0 if endianness is big, 1 otherwise
+ */
+int get_endianness(void)
+{
+	int num = 2;
+
+	return ((num & 1)? 0 : 1);
+}
