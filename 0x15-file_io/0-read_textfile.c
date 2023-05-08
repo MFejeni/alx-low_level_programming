8ef2606a5694a8ccabefa76be8c7b78812281b48
MFejeni <mfejeni@gmail.com>
2023-05-08 16:28:38 -0700

    Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads the textfile and prints it to POSIX stdio
+ * @filename: the file to be read and printed to POSIX
+ * @letters: number of letters the function should read and print to POSIX
+ * Return: number of letters that are read and printed
+ */
+
+ssize_t read_t read_textfile(const char *filename, size_t letters)
+{
+	int filecheck, read_num, write_num;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	filecheck = open(filename, O_RDONLY);
+	
+	if (filecheck == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	read_num = read(filecheck, buffer, letters);
+	write_num = write(STOUT_FILENO, buffer, read_num);
+	close(filecheck);
+	free(buffer);
+	return (write_num);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+Read and write files using open file, that actually produces other files is implemented using standard output and input libraties of C, creating files and reading already created files.
